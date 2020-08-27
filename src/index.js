f77e8ed54cdceb70ebc0d0ece6b8d27c64eda74b
junko911 <junkotahara911@gmail.com>
2020-08-27 14:14:30 -0400

    Fetch toys and create cards

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.getElementById('toy-collection')
+  
+  const renderToys = (collection) => {
+    for(const toy of collection) {
+      renderToy(toy)
+    }
+  }
+  
+  const renderToy = (toyObj) => {
+    const toyDiv = document.createElement('div')
+    toyDiv.className = 'card'
+    toyDiv.innerHTML = `
+      <h2>${toyObj.name}</h2>
+      <img src="${toyObj.image}" class="toy-avatar">
+      <p>${toyObj.likes} Likes</p>
+      <button class="like-btn">Like <3</button>
+    `
+    toyCollection.append(toyDiv)
+  }
+
+  fetch('http://localhost:3000/toys')
+    .then(toy => toy.json())
+    .then(toys => renderToys(toys))
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
