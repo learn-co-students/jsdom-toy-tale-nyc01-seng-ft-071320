6161d9e5c2e4fd0c25674aa619d3371c729b5fb3
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-27 12:42:45 -0500

    get data and render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 let addToy = false;
 
+
 document.addEventListener("DOMContentLoaded", () => {
+  function renderToys(arrayOfObjects) {
+    for(let object of arrayOfObjects) {
+      let name = object.name
+      let image = object.image
+      let likes = object.likes
+      makeToyCard(name, image, likes)
+    }
+  }
+
+  let cardsDiv = document.querySelector('#toy-collection')
+
+  function makeToyCard(name, image, likes) {
+    let cardDiv = document.createElement('div')
+    cardDiv.className = 'card'
+    cardDiv.innerHTML = `
+      <h2>${name}</h2>
+      <img src=${image} class="toy-avatar"/>
+      <p>${likes} likes</p>
+      <button class="like-button">Like<3</button>
+    `
+    cardsDiv.append(cardDiv)
+  }
+
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(toys => renderToys(toys))
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  document.addEventListener('submit', function(e) {
+    e.preventDefault()
+  })
 });
