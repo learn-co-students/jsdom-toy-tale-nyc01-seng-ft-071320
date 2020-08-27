f283bf8d0d3c4030707ad7013b24c3893bd229b9
stackguardian <mferdous98@aol.com>
2020-08-27 15:26:03 -0400

    adding toys

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const fields = document.querySelector('form').children
+  
   document.addEventListener("click", (e) => {
+    e.preventDefault()
     // hide & seek with the form
     const button = e.target
     if (button.matches("#new-toy-btn")) {
@@ -14,15 +17,35 @@ document.addEventListener("DOMContentLoaded", () => {
         toyFormContainer.style.display = "none";
       }
     } else if(button.matches(".submit")){
-      
-
+      const nameField = fields[1].value
+      const imgField = fields[3].value
+      createToy(nameField, imgField)
     }
-
-
-
     
   });
-  
+  function createToy(name, imageURL, like=0) {
+    const collection = document.getElementById('toy-collection');
+      const div = document.createElement('div');
+      div.className = "card";
+
+      const h2 = document.createElement('h2');
+      h2.innerText = name;
+
+      const img = document.createElement('img');
+      img.src = imageURL;
+
+      const p = document.createElement('p');
+      p.innerText = like;
+
+      const likeBtn = document.createElement('button');
+      likeBtn.className = "like-btn"; 
+      
+      div.append(h2);
+      div.append(img);
+      div.append(p);
+      div.append(likeBtn);
+      collection.append(div);
+    }
 
   const getToys = () => {
     fetch('http://localhost:3000/toys')
