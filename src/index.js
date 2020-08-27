256fac28803052ab33fbd75a362ac87b4bf5992b
github username <joshmrallen@gmail.com>
2020-08-27 16:05:55 -0400

    added likes and started patch fetch

--- src/index.js
+++ src/index.js
@@ -17,12 +17,36 @@ document.addEventListener("DOMContentLoaded", () => {
         toyFormContainer.style.display = "none";
       }
     } else if(button.matches(".submit")){
-      const nameField = fields[1].value
-      const imgField = fields[3].value
-      createToy(nameField, imgField)
+        e.preventDefault()
+        const nameField = fields[1].value
+        const imgField = fields[3].value
+        createToy(nameField, imgField)
+        fields[0].parentElement.reset()
+    } else if(button.matches(".like-btn")) {
+        let likesP = parseInt(button.previousSibling.innerText, 10)
+        likesP = likesP + 1
+        button.previousSibling.innerText = likesP
+
+        let configObj = {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify({
+            "likes": likesP
+          })
+        }
+
+        // fetch(`http://localhost:3000/toys/${}`, configObj)
+        //   .then(response => response.json())
+        //   .then(success => console.log("successfully updated likes"))
+        //   .catch(error => console.log(error.message))
     }
     
   });
+
+
   function createToy(name, imageURL, like=0) {
     const collection = document.getElementById('toy-collection');
       const div = document.createElement('div');
@@ -56,27 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(toyCollection);
 
         for(const toy of toyCollection) {
-          const collection = document.getElementById('toy-collection');
-          const div = document.createElement('div');
-          div.className = "card";
-
-          const h2 = document.createElement('h2');
-          h2.innerText = toy.name;
-
-          const img = document.createElement('img');
-          img.src = toy.image;
-
-          const p = document.createElement('p');
-          p.innerText = toy.likes;
-
-          const likeBtn = document.createElement('button');
-          likeBtn.className = "like-btn"; 
-          
-          div.append(h2);
-          div.append(img);
-          div.append(p);
-          div.append(likeBtn);
-          collection.append(div);
+          createToy(toy.name, toy.image, toy.likes);
         }
 
       })
